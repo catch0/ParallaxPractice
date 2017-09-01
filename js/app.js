b9d4f3c12f7fd13aa8a6afcd755ccf97d75576a4
Spence Peacock <spencerpeacock@gmail.com>
2017-09-01 15:16:34 -0500

    fixed scrolling and title box

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-$(document).ready(function){
-  setBindings();
-}
+$(document).ready(function(){
+    setBindings();
+});
 
 function setBindings(){
   $("nav a").click(function(e){
-    alert('button id + e.currentTarget.id');
+    e.preventDefault();
+    var sectionID = e.currentTarget.id + "Section";
+
+    $('html body').animate({
+      scrollTop: $("#" + sectionID).offset().top
+    }, 1000)
   })
 }
