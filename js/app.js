d1f1effa5f3b234aa2b2f294d0be4a6024f2ce5f
Spence Peacock <spencerpeacock@gmail.com>
2017-09-05 15:23:23 -0500

    tweeks to textillate

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     setBindings();
+    rollShit();
 });
 
 function setBindings(){
@@ -11,4 +12,32 @@ function setBindings(){
       scrollTop: $("#" + sectionID).offset().top
     }, 1000)
   })
+}
+
+// var $tlt = $('.tlt').textillate({
+//     autoStart: false,
+//     in: { effect: 'hinge', callback: function(){
+//     $('.tlt').textillate('out');
+//     }},
+//     out:{effect: 'rollIn'}
+// });
+//
+// $('.tlt').hover(function () {
+//     $tlt.textillate('start');
+// });
+
+
+
+
+function rollShit(){
+  $(".tlt").hover(function(){
+    $(this).textillate({
+
+      in:{ delay: 200,effect: 'hinge', shuffle:true,
+      callback: function(){
+                $('.tlt').textillate('out');
+                }},
+      out: {effect: 'rollIn'}
+   });
+  });
 }
